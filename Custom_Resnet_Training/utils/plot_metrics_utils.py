4945b06b192ddddde45c1dba294b8751d9e1f8c1
nkanungo <40553830+nkanungo@users.noreply.github.com>
2021-07-07 16:35:03 +0530

    Update plot_metrics_utils.py

--- Custom_Resnet_Training/utils/plot_metrics_utils.py
+++ Custom_Resnet_Training/utils/plot_metrics_utils.py
@@ -11,3 +11,19 @@ def plot_loss_accuracy(train_losses, train_acc, test_losses, test_acc):
     axs[0, 1].set_title("Test Loss")
     axs[1, 1].plot(test_acc)
     axs[1, 1].set_title("Test Accuracy")
+
+def plot_accuracy(train_acc, test_acc):	
+    fig, axs = plt.subplots(figsize=(5,5))
+    axs.plot(train_acc, label="Train Accuracy")
+    axs.plot(test_acc, label="Test Accuracy")
+    axs.legend()
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.show()
+	
+def plot_max_lr_vs_test_accuracy(max_lr_list,test_accuracy_list):
+    fig, axs = plt.subplots(figsize=(5,5))
+    axs.plot(max_lr_list,test_accuracy_list)
+    plt.xlabel("Maximum Learning Rate")
+    plt.ylabel("Test Accuracy")
+    plt.show()
